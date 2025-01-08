a3faf79e3f7e5229d6de4ca2dc21f9b9780c06df
Ahmed <koukiahmed668@gmail.com>
2025-01-08 09:04:29 +0100

    added loading animation

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,16 +1,27 @@
-import { Component} from '@angular/core';
-
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
 
- 
+  constructor(private renderer: Renderer2) {}
 
- 
-  
+  ngOnInit(): void {
+    this.setupPreloader();
+  }
 
+  private setupPreloader(): void {
+    const preloader = document.getElementById('js-preloader');
+    if (preloader) {
+      window.addEventListener('load', () => {
+        // Ensure the preloader shows for at least 1 second
+        setTimeout(() => {
+          this.renderer.addClass(preloader, 'loaded');
+        }, 500); // 1-second delay
+      });
+    }
+  }
 }
